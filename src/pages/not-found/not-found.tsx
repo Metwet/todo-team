d346e33fd246da0c0d5f0b81a5aef0acfc30ab65
Metwet <tselm.matv@gmail.com>
2024-07-25 23:39:39 +0300

    feat: create page 404

--- src/pages/not-found/not-found.tsx
+++ src/pages/not-found/not-found.tsx
@@ -1,7 +1,28 @@
-import { Box } from "@mui/material"
+import { Box, Container, Typography } from "@mui/material";
+import styles from "./not-found.module.scss";
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
-    return <Box>Page not found</Box>
-}
+  return (
+    <Container maxWidth="lg">
+      <Typography
+        variant="h2"
+        gutterBottom
+        className={styles.title}
+        sx={{ mt: 10, fontSize: 104 }}
+      >
+        404
+      </Typography>
+      <Box className={styles.box} sx={{ fontSize: 34 }}>
+        Page not found
+      </Box>
+      <Box className={styles.box} sx={{ fontSize: 34 }}>
+        <Link to="/" className={styles.link}>
+          HOME PAGE
+        </Link>
+      </Box>
+    </Container>
+  );
+};
 
 export default NotFound;
